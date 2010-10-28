90246bf303aef68b53806d54d13eae553b87c32a
noobnl <shareefalis@gmail.com>
2010-10-27 23:25:36 -0700

    changed naming convention

--- products/cyanogen_epic.mk
+++ products/cyanogen_epic.mk
@@ -1,5 +1,5 @@
 # Inherit device configuration for Epic.
-$(call inherit-product, device/samsung/SPH-D700/full_epic.mk)
+$(call inherit-product, device/samsung/epic/full_epic.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common.mk)
@@ -12,12 +12,12 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 #
 PRODUCT_NAME := cyanogen_epic
 PRODUCT_BRAND := Samsung
-PRODUCT_DEVICE := SPH-D700
-PRODUCT_MODEL := SPH-D700
+PRODUCT_DEVICE := epic
+PRODUCT_MODEL := Samsung-SPH-D700
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D700 TARGET_DEVICE=SPH-D700 TARGET_BOOTLOADER_BOARD_NAME=SPH-D700 BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
 
-# Extra Epic(via Vibrant) overlay
+# Extra Galaxy S overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/galaxys
 
 #
