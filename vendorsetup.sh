b416986f7fab880e777a37ba713ff3c03b5f3323
Wes Garner <wesgarner@gmail.com>
2010-10-28 08:01:02 -0500

    vendorsetup: give GalaxyS lunch combos their own section
    Change-Id: If75e07f7b1eba910ca462cfc117834a37f7f0302

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
 add_lunch_combo cyanogen_bravo-eng
 add_lunch_combo cyanogen_bravoc-eng
-add_lunch_combo cyanogen_captivate-eng
 add_lunch_combo cyanogen_dream_sapphire-eng
 add_lunch_combo cyanogen_espresso-eng
 add_lunch_combo cyanogen_hero-eng
@@ -11,7 +10,6 @@ add_lunch_combo cyanogen_liberty-eng
 add_lunch_combo cyanogen_passion-eng
 add_lunch_combo cyanogen_sholes-eng
 add_lunch_combo cyanogen_supersonic-eng
-add_lunch_combo cyanogen_vibrant-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_one-eng
 add_lunch_combo cyanogen_buzz-eng
@@ -20,6 +18,6 @@ add_lunch_combo cyanogen_glacier-eng
 add_lunch_combo cyanogen_harmony-eng
 add_lunch_combo cyanogen_epic-eng
 add_lunch_combo cyanogen_captivate-eng
-
+add_lunch_combo cyanogen_vibrant-eng
 
 PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
