56a4fa30ce0fe82cdd731fe0a9395f077a48f97b
Daniel Hillenbrand <info@codeworkx.de>
2010-10-30 10:48:08 -0700

    added galaxys (GT-I9000)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -20,8 +20,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_glacier.mk \
     $(LOCAL_DIR)/cyanogen_harmony.mk \
     $(LOCAL_DIR)/cyanogen_epic.mk \
-    $(LOCAL_DIR)/cyanogen_i9000.mk \
-    $(LOCAL_DIR)/cyanogen_epic.mk \
     $(LOCAL_DIR)/cyanogen_captivate.mk \
-    $(LOCAL_DIR)/cyanogen_fascinate.mk
-
+    $(LOCAL_DIR)/cyanogen_fascinate.mk \
+    $(LOCAL_DIR)/cyanogen_galaxys.mk
