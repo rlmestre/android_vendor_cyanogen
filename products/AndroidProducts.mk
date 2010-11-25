6e36a7e1d5e8f28edc7424059d331425fe4e38c0
Codeworkx <info@codeworkx.de>
2010-11-25 21:16:51 +0100

    merged with mainline, did some cleanup

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,4 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cyanogen_inc.mk \
     $(LOCAL_DIR)/cyanogen_bravo.mk \
     $(LOCAL_DIR)/cyanogen_bravoc.mk \
     $(LOCAL_DIR)/cyanogen_buzz.mk \
@@ -9,23 +8,25 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_espresso.mk \
     $(LOCAL_DIR)/cyanogen_fascinate.mk \
     $(LOCAL_DIR)/cyanogen_galaxys.mk \
+    $(LOCAL_DIR)/cyanogen_glacier.mk \
+    $(LOCAL_DIR)/cyanogen_harmony.mk \
     $(LOCAL_DIR)/cyanogen_hero.mk \
     $(LOCAL_DIR)/cyanogen_heroc.mk \
+    $(LOCAL_DIR)/cyanogen_inc.mk \
     $(LOCAL_DIR)/cyanogen_legend.mk \
     $(LOCAL_DIR)/cyanogen_liberty.mk \
+    $(LOCAL_DIR)/cyanogen_one.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_vibrant.mk \
-    $(LOCAL_DIR)/cyanogen_legend.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
-    $(LOCAL_DIR)/cyanogen_one.mk \
-    $(LOCAL_DIR)/cyanogen_z71.mk \
-    $(LOCAL_DIR)/cyanogen_glacier.mk \
-    $(LOCAL_DIR)/cyanogen_harmony.mk \
-    $(LOCAL_DIR)/cyanogen_epic.mk \
-    $(LOCAL_DIR)/cyanogen_captivate.mk \
-    $(LOCAL_DIR)/cyanogen_fascinate.mk \
-    $(LOCAL_DIR)/cyanogen_galaxys.mk
-    $(LOCAL_DIR)/cyanogen_vision.mk
+    $(LOCAL_DIR)/cyanogen_z71.mk
+
+
+
+
+
+
+
 
