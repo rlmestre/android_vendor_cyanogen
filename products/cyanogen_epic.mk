d6b8e3775e0eb73e6377f597817ed7857ece15fa
noobnl <shareefalis@gmail.com>
2010-12-25 04:32:53 -0600

    6.2.0BETA0

--- products/cyanogen_epic.mk
+++ products/cyanogen_epic.mk
@@ -28,7 +28,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-Epic
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.0-RC0-Epic
+        ro.modversion=CyanogenMod-6.2.0-BETA0-Epic
 endif
 
 #
