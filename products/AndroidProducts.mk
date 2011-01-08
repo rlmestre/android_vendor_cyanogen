1f731d890ba7092b187c24629c009a38ebbc4e42
Forcystos <forcystos@hotmail.com>
2011-01-07 19:27:16 -0800

    remove other sgs

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cyanogen_captivate.mk \
     $(LOCAL_DIR)/cyanogen_epic.mk \
-    $(LOCAL_DIR)/cyanogen_fascinate.mk \
-    $(LOCAL_DIR)/cyanogen_galaxys.mk \
-    $(LOCAL_DIR)/cyanogen_vibrant.mk
+    $(LOCAL_DIR)/cyanogen_galaxys.mk
